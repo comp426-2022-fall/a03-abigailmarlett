0fdf3b3bf2059f10d1dcde2f9ae2960f0fd9b8ce
Abigail Marlett <amarlett@email.unc.edu>
2022-11-28 10:58:13 -0500

    roll fixed

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import minimist from "minimist"
-import { roll } from '../lib/roll.js'
+import roll from "/lib/roll.js"
 
 const args = minimist(process.argv.slice(2));
 
@@ -8,4 +8,4 @@ let sides = args.sides;
 let dice = args.dice;
 let rolls = args.rolls;
 
-console.log(JSON.stringify(roll(sides, dice, rolls)))
+console.log(JSON.stringify(roll(sides, dice, rolls)));
