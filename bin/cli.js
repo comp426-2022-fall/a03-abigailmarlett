937407a6062d101b785d6d80ab252283ec447d34
Abigail Marlett <amarlett@email.unc.edu>
2022-11-29 14:10:05 -0500

    error

--- bin/cli.js
+++ bin/cli.js
@@ -4,8 +4,8 @@ import roll from "../lib/roll.js"
 
 const args = minimist(process.argv.slice(2));
 
-let sides = args.sides;
-let dice = args.dice;
+let sides = args.sides || 6;
+let dice = args.dice || 2;
 let rolls = args.rolls;
 
 console.log(JSON.stringify(roll(sides, dice, rolls)));
